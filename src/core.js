30852f7cab713db58b944a641ed38a264017d293
Harish R <rhrish@gmail.com>
2016-06-21 15:34:33 +0530

    Vote function modified so that only a part of state is sent to the function instead of the whole state tree

--- src/core.js
+++ src/core.js
@@ -29,5 +29,5 @@ export function next(state) {
 }
 
 export function vote(state, entry) {
-  return state.updateIn(['vote','tally',entry],0, tally => tally+1);
+  return state.updateIn(['tally',entry],0, tally => tally+1);
 }
